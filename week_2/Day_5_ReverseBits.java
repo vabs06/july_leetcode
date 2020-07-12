2326978c7b269d4cc3c39baea6696e8eeccbaa38
Vaibhav Malviya <vabs.malviya@gmail.com>
2020-07-12 16:06:13 +0530

    Week-2/Day-5

---
+++ week_2/Day_5_ReverseBits.java
@@ -0,0 +1,6 @@
+//	Question Link: https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/545/week-2-july-8th-july-14th/3388/
+class Day_5_ReverseBits {
+	public int reverseBits(int n) {
+	        return Integer.reverse(n);
+	}
+}
